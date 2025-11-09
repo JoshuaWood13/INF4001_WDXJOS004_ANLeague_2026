e672d167394ef971f9f7f4d8ded5df3224440e77
Joshua <st10296167@vcconnect.edu.za>
2025-11-09 23:22:33 +0200

    Removed unused service files

--- Services/Email/IEmailService.cs
+++
@@ -1,9 +0,0 @@
-//using MatchEntity = INF4001_WDXJOS004_ANLeague_2026.Models.Entities.Match;
-
-//namespace INF4001_WDXJOS004_ANLeague_2026.Services.Email
-//{
-//    public interface IEmailService
-//    {
-//        Task SendMatchResultNotificationAsync(MatchEntity match, string homeCountryEmail, string awayCountryEmail);
-//    }
-//}
